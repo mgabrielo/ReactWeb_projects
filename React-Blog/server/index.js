49cc426de9061be0423f51fbea3fbc4ba6da80fc
mgabrielo <121618872+mgabrielo@users.noreply.github.com>
2023-08-09 21:42:34 +0100

    Update index.js

--- React-Blog/server/index.js
+++ React-Blog/server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes)
 
-const CONNECT_URL = 'mongodb+srv://greycoinz:greyscott99@cluster0.upky5y7.mongodb.net/react-blog'
+const CONNECT_URL = 'mongodb+srv://greycoinz:@cluster0.upky5y7.mongodb.net/react-blog'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>
