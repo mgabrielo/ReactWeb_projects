4bdbaf7c5058069b1f94e92a1311da8996a2a45e
mgabrielo <121618872+mgabrielo@users.noreply.github.com>
2023-08-25 07:03:40 +0100

    Delete React-Redux-Firebase/eshop directory

--- React-Redux-Firebase/eshop/src/pages/contact/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Contact.module.scss'
-const Contact = () => {
-    return (
-        <div>Contact</div>
-    )
-}
-
-export default Contact
--- React-Redux-Firebase/eshop/src/pages/orderHistory/OrderHistory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './OrderHistory.module.scss'
-const OrderHistory = () => {
-    return (
-        <div>OrderHistory</div>
-    )
-}
-
-export default OrderHistory
