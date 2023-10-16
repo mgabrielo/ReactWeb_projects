b283d8383b19a76e31f27bde21dfed3528117ba3
Scott Nitro <scottnitro1@gmail.com>
2023-10-16 19:03:52 +0100

    9th commit

--- estate-app/api/index.js
+++ estate-app/api/index.js
@@ -6,7 +6,11 @@ import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config();
+
+const __dirname = path.resolve();
+
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
@@ -22,6 +26,11 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
